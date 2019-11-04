ecf94b6386262322a038de9afead5c97ea455369
Moutoussamy <57101711+Moutoussamy@users.noreply.github.com>
2019-11-04 15:02:08 +0100

    comments

--- get_system_size.py
+++ get_system_size.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # ~*~ coding:utf-8 ~*~
 
+"""
+This allow to calculate the system size
+print the cellbasis for NAMD
+"""
 
 __author__ = "Emmanuel Edouard MOUTOUSSAMY"
 __version__  = "1.0.0"
@@ -16,22 +20,27 @@ import sys
 
 
 def get_coord(pdb):
+	"""
+	collect the coordinate from a PDB file
+	:param pdb: a pdb file format: http://cupnet.net/pdb-format/
+	:return:a numpy array containing the coordinate of your system
+	"""
 
 	flag = 0
 
 	with open(sys.argv[1],"r") as input_file:
 		for line in input_file:
 			if line[0:4] == "ATOM":
-				x = float(line[30:38])
-				y = float(line[38:46])
-				z = float(line[46:54])
+				x = float(line[30:38]) #get x coord
+				y = float(line[38:46]) #get y coord
+				z = float(line[46:54]) #get z coord
 
 				if flag ==0:
-					coord = np.array([x,y,z])
+					coord = np.array([x,y,z]) #initinialze numpy array
 					flag = 1
 
 				else:
-					coord = np.vstack((coord,[x,y,z]))
+					coord = np.vstack((coord,[x,y,z])) #numpy aray extension
 
 
 
@@ -40,13 +49,13 @@ def get_coord(pdb):
 
 def CoordInfo(coord):
 	"""
-
-	:param coord:
-	:return:
+	calculate the geometrical center of the system and the system size
+	:param coord: a numpy array containing the coordinate of your system
+	:return: system size in X, system size in Y, system size in Z, center of geometry (COG) x,COG Y, COG Z
 	"""
-	argx = max(coord[:,0]) - min(coord[:,0])
-	argy = max(coord[:,1]) - min(coord[:,1])
-	argz = max(coord[:,2]) - min(coord[:,2])
+	argx = max(coord[:,0]) - min(coord[:,0]) #get the larger x dist
+	argy = max(coord[:,1]) - min(coord[:,1]) #get the larger y dist
+	argz = max(coord[:,2]) - min(coord[:,2]) #get the larger z dist
 
 	centerx= np.mean(coord[:,0])
 	centery = np.mean(coord[:,1])
